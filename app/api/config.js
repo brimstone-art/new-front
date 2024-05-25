646e0e6a2c4aa7025c1a174e946e8a1e18aa7060
brimstone-art <149525088+brimstone-art@users.noreply.github.com>
2024-05-25 17:16:05 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,7 @@
-export const BASE_URL = 'https://br-domain.nomoredomainswork.ru',
+export const BASE_URL = ['https://br-domain.nomoredomainswork.ru',
                          'https://br-domain.nomoredomainswork.ru/api/games',
-                         'https://br-domain.nomoredomainswork.ru/api/categories';
+                         'https://br-domain.nomoredomainswork.ru/api/categories'
+                        ]
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
